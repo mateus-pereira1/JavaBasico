b185c02e7b003da7bb217178e90e60fe09c72c5c
mateus-pereira1 <147281084+mateus-pereira1@users.noreply.github.com>
2026-05-10 13:03:48 -0300

    Aula 06 e 07.
    Adicionado os arquivos referentes as aulas 06 (Teórica e prática) e 07 (Teórica).

---
+++ aula007/Media/src/media/Media.java
@@ -0,0 +1,15 @@
+
+package media;
+
+public class Media {
+
+    public static void main(String[] args) {
+        int n1 = 5;
+        int n2 = 3;
+        float media = (n1+n2)/2;
+        
+        System.out.println("A média é igual a : "+media);
+        
+    }
+    
+}
